fa7e7b822ab27b5ba284b119634df2e639089b5c
pabogdan <dwTheresnosugar2013>
2015-10-20 10:35:23 +0100

    updated setup.py using newer versions of nengo and nengo_spinnaker and including nengo_gui as dependency

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     author_email='pab@cs.man.ac.uk',
     description='Spiking neural network vision system using a silicon sensor retina running on SpiNNaker',
     # Requirements
-    dependency_links = ['https://github.com/nengo/nengo/tarball/23107fe#egg=nengo-2.1.0.dev0',
-                        'https://github.com/pabogdan/nengo_spinnaker/tarball/master#egg=nengo-spinnaker-0.2.4'],
+    dependency_links = ['https://github.com/nengo/nengo/tarball/master#egg=nengo-2.1.0.dev0',
+                        'https://github.com/project-rig/nengo_spinnaker/tarball/master#egg=nengo-spinnaker-0.2.5',
+                        'https://github.com/nengo/nengo_gui/tarball/master'],
 
     install_requires=["nengo==2.1.0.dev0", "rig>=0.5.3, <1.0.0",
-                      "bitarray>=0.8.1, <1.0.0", "nengo-spinnaker==0.2.4"],
+                      "bitarray>=0.8.1, <1.0.0", "nengo-spinnaker==0.2.5", "nengo_gui>=0.1.4"],
     classifiers=[
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
